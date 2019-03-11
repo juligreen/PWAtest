99d6cd61e48db9f4f54ed9fea497656de4bbaf22
julius.dehner <julius.dehner@is2.de>
2019-03-11 17:23:49 +0100

    Ask for notification permission

--- scripts/main.js
+++ scripts/main.js
@@ -7,5 +7,20 @@ window.onload = () => {
 	}
 	Notification.requestPermission(function (status) {
 		console.log('Notification permission status:', status);
-	});
+	}).then(function(){
+		displayNotification()
+	})
+
+
+	function displayNotification() {
+		if (Notification.permission === 'granted') {
+			navigator.serviceWorker.getRegistration().then(function (reg) {
+				reg.showNotification('Hello world!');
+			});
+		}
+		else {
+			console.log('no permission')
+		}
+	}
+
 };
